bb49eca7ddc403f0c7ad42d4be1fcb471b44dfce
Nahid Ferdous <nahid.mferdous@gmail.com>
2022-11-20 03:32:07 -0600

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,19 @@
+
+def upate_count(letters, counts_d):
+    for c in letters:
+        if c in counts_d:
+            counts_d[c]=counts_d[c]+1
+        else:
+            counts_d[c]=1
+    return counts_d
+
+
+counts_ds={"a":2,"b":3}
+print(upate_count("aaab",counts_ds))
+print(counts_ds['a'])
+print(counts_ds['b'])
+
+counts_ds={}
+print(upate_count("aaab",counts_ds))
+print(counts_ds['a'])
+print(counts_ds['b'])
